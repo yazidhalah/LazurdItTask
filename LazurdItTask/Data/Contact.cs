b314b50d09b6fd1e95bc93aa7f067f83210e0e74
yazid maverick <yazidhalah1995@gmail.com>
2024-05-07 03:41:29 +0300

    first commit

---
+++ LazurdItTask/Data/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LazurdItTask.Data
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name is required")]
+        [MinLength(1)]
+        public string FirstName { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name is required")]
+        [MinLength(1)]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ LazurdItTask/Services/ContactService.cs
@@ -0,0 +1,42 @@
+﻿using LazurdItTask.Data;
+
+namespace LazurdItTask.Services
+{
+    public class ContactService
+    {
+        private List<Contact> contacts = new();
+        private int nextId = 1;
+
+        public List<Contact> GetAll()
+        {
+            return contacts;
+        }
+        public Contact GetById(int id)
+        {
+            if (contacts != null)
+            {
+                var contact = contacts.OrderBy(x => x.Id).FirstOrDefault(c => c.Id == id);
+                return contact != null ? contact : new Contact();
+            }
+            else
+            {
+                return new Contact();
+            }
+        }
+        public void Add(Contact contact)
+        {
+            contact.Id = nextId++;
+            contacts.Add(contact);
+        }
+        public void Update(Contact contact)
+        {
+            var index = contacts.FindIndex(c => c.Id == contact.Id);
+            if (index != -1)
+                contacts[index] = contact;
+        }
+        public void Delete(int id)
+        {
+            contacts.RemoveAll(c => c.Id == id);
+        }
+    }
+}
